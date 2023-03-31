ecfedd39abc739d65a8adf4051c48dc918e3d46c
Muneeb Ali <69442174+muneebalichishti01@users.noreply.github.com>
2023-03-30 22:40:38 -0600

    fixed syntax error for Animal.java

--- JAVA/animal.java
+++ JAVA/animal.java
@@ -1,24 +1,23 @@
-// package edu.ucalgary.oop;
-// package ENSF380_Final_Project;
+public class Animal {
+    private final int ANIMALID;
+    private final String ANIMALNICKNAME;
+    private final String ANIMALSPECIES;
 
-public class Animal{
-	private final int ANIMALID;
-	private final String ANIMALNICKNAME;
-	private final String ANINALSPECIES;
-	
-	public Animal(int animalID, String Nickname, String Species){
-		this.ANIMALID= animalID;
-		this.ANIMALNICKNAME = Nickname;
-		this.ANINALSPECIES= Species;
-	}
-	public int getAnimalID(){
-		return this.ANIMALID;
-	}
-	public String getAnimalNickName(){
-		return this.ANIMALNICKNAME;
-	}
-	public String getAnimalSpecies(){
-		return this.ANINALSPECIES;
-	}
-	
+    public Animal(int animalID, String Nickname, String Species){
+        this.ANIMALID = animalID;
+        this.ANIMALNICKNAME = Nickname;
+        this.ANIMALSPECIES = Species;
+    }
+
+    public int getAnimalID(){
+        return this.ANIMALID;
+    }
+
+    public String getAnimalNickName(){
+        return this.ANIMALNICKNAME;
+    }
+
+    public String getAnimalSpecies(){
+        return this.ANIMALSPECIES;
+    }
 }
