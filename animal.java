9a7b09929da397d0dca97db4858c7f6b59d771f4
moe3344 <mohamad.hammoud1@ucalgary.ca>
2023-03-30 22:09:04 -0600

    animal class

---
+++ animal.java
@@ -0,0 +1,21 @@
+package edu.ucalgary.oop;
+public class Animal{
+	private final int ANIMALID;
+	private final String ANIMALNICKNAME;
+	private final String ANINALSPECIES;
+	public Animal(int animalID, String Nickname, String Species){
+		this.ANIMALID= animalID;
+		this.ANIMALNICKNAME = Nickname;
+		this.ANINALSPECIES= Species;
+	}
+	public int getAnimalID(){
+		return this.ANIMALID;
+	}
+	public String getAnimalNickName(){
+		return this.ANIMALNICKNAME;
+	}
+	public String getAnimalSpecies(){
+		return this.ANINALSPECIES;
+	}
+	
+}
